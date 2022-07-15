8715db1e28f60868c0f293bb59f4ed71e6bb886b
ollylll97lll <58421531+ollylll97lll@users.noreply.github.com>
2022-07-15 10:58:37 +0700

    First User Story & Setting Up Project
    -Finish First User Story (Register User)
    -Setup Docker using docker-compose.yml
    -TODO: Refine Exceptions catched from constraint annotations.

--- src/main/java/com/example/springrestapi/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.springrestapi.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.springrestapi.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-    public User findByEmail(String email);
-}
